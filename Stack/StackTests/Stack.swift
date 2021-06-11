bd7eabf301317d2559b5553221d1bb3071aeb31a
Suzhenyu <983424749@qq.com>
2021-06-11 17:49:03 +0800

    优化 Stack

--- Stack/StackTests/Stack.swift
+++ Stack/StackTests/Stack.swift
@@ -1,33 +1,34 @@
 public struct Stack<T> {
     fileprivate var array = [T]()
     
+    // 平均时间复杂度为 O(1)
     public mutating func push(_ newElement: T) {
         array.append(newElement)
     }
     
+    // 时间复杂度为 O(1)
     @discardableResult
     public mutating func pop() -> T? {
         return array.popLast()
     }
     
+    // 时间复杂度为 O(1)
+    public func peek() -> T? {
+        return array.last
+    }
+    
+    // 时间复杂度为 O(1)
     public var isEmpty: Bool {
         return array.isEmpty
     }
     
+    // 时间复杂度为 O(1)
     public var count: Int {
         return array.count
     }
     
+    // 时间复杂度为 O(1)
     public var top: T? {
         return array.last
     }
 }
-
-//extension Stack: Sequence {
-//    public func makeIterator() -> AnyIterator<T> {
-//        var curr = self
-//        return AnyIterator {
-//            return curr.pop()
-//        }
-//    }
-//}
